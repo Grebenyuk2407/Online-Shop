09e5bc175dea5e7bff95edbf8f74f824c3255ef7
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-04-15 20:34:44 +0300

    Create BrandAdapter

--- app/src/main/java/dev/androidbroadcast/onlineshop/activity/MainActivity.kt
+++ app/src/main/java/dev/androidbroadcast/onlineshop/activity/MainActivity.kt
@@ -2,17 +2,14 @@ package dev.androidbroadcast.onlineshop.activity
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
+import dev.androidbroadcast.onlineshop.Adapter.BrandAdapter
 import dev.androidbroadcast.onlineshop.Models.SliderModel
-import dev.androidbroadcast.onlineshop.R
-import dev.androidbroadcast.onlineshop.SliderAdapter
+import dev.androidbroadcast.onlineshop.Adapter.SliderAdapter
 import dev.androidbroadcast.onlineshop.databinding.ActivityMainBinding
 import dev.androidbroadcast.onlineshop.viewModel.MainViewModel
 
@@ -24,7 +21,8 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-initBanner()
+        initBanner()
+        initBrand()
 
 
     }
@@ -37,23 +35,35 @@ initBanner()
         })
         viewModel.loadBanners()
     }
-    private fun banners(images:List<SliderModel>){
+
+    private fun banners(images: List<SliderModel>) {
         binding.viewpagerSlider.adapter = SliderAdapter(images, binding.viewpagerSlider)
-        binding.viewpagerSlider.clipToPadding=false
-        binding.viewpagerSlider.clipChildren=false
+        binding.viewpagerSlider.clipToPadding = false
+        binding.viewpagerSlider.clipChildren = false
         binding.viewpagerSlider.offscreenPageLimit = 3
-        binding.viewpagerSlider.getChildAt(0).overScrollMode=RecyclerView.OVER_SCROLL_NEVER
+        binding.viewpagerSlider.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
 
 
         val compositePageTransformer = CompositePageTransformer().apply {
             addTransformer(MarginPageTransformer(40))
         }
         binding.viewpagerSlider.setPageTransformer(compositePageTransformer)
-        if(images.size > 1){
+        if (images.size > 1) {
             binding.dotIndicator.visibility = View.VISIBLE
             binding.dotIndicator.attachTo(binding.viewpagerSlider)
         }
 
 
+    }
+
+    private fun initBrand() {
+        binding.progressBarBrand.visibility = View.VISIBLE
+        viewModel.brands.observe(this, Observer {
+            binding.viewBrand.layoutManager =
+                LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            binding.viewBrand.adapter = BrandAdapter(it)
+            binding.progressBarBrand.visibility = View.GONE
+        })
+        viewModel.loadBrands()
     }
 }
--- app/src/main/java/dev/androidbroadcast/onlineshop/viewModel/MainViewModel.kt
+++ app/src/main/java/dev/androidbroadcast/onlineshop/viewModel/MainViewModel.kt
@@ -7,6 +7,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import dev.androidbroadcast.onlineshop.Models.BrandModel
 import dev.androidbroadcast.onlineshop.Models.SliderModel
 
 class MainViewModel():ViewModel() {
@@ -14,7 +15,10 @@ class MainViewModel():ViewModel() {
     private val firebaseDatabase =  FirebaseDatabase.getInstance()
 
     private val _banner = MutableLiveData<List<SliderModel>>()
+    private val _brand = MutableLiveData<MutableList<BrandModel>>()
 
+
+    val brands :LiveData<MutableList<BrandModel>> = _brand
     val banners: LiveData<List<SliderModel>> = _banner
 
 
@@ -31,6 +35,26 @@ class MainViewModel():ViewModel() {
                 }
                 _banner.value = lists
 
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+    fun loadBrands(){
+        val Ref = firebaseDatabase.getReference("Category")
+        Ref.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<BrandModel>()
+                for (childSnapshot in snapshot.children){
+                    val list = childSnapshot.getValue(BrandModel::class.java)
+                    if(list != null){
+                        lists.add(list)
+                    }
+                }
+                _brand.value = lists
             }
 
             override fun onCancelled(error: DatabaseError) {
