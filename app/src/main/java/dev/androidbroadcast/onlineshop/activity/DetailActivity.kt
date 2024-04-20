e886db1882487b5a808407d3c62d39869f1e5805
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-04-20 17:37:38 +0300

    finish detail_xml

---
+++ app/src/main/java/dev/androidbroadcast/onlineshop/activity/DetailActivity.kt
@@ -0,0 +1,15 @@
+package dev.androidbroadcast.onlineshop.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dev.androidbroadcast.onlineshop.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
