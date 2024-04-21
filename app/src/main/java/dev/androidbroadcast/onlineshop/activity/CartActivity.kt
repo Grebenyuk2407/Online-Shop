b68f830406e8c878454805a81460ca270cede5c8
Anton Grebenyuk <antongrebenyuk1231@gmail.com>
2024-04-21 10:56:25 +0300

    finish activity_cart_xml

---
+++ app/src/main/java/dev/androidbroadcast/onlineshop/activity/CartActivity.kt
@@ -0,0 +1,12 @@
+package dev.androidbroadcast.onlineshop.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.androidbroadcast.onlineshop.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
